dccc3c46fd09967ed5589170be4144713a4828b6
Viet Le <vietlq85@gmail.com>
2016-08-07 21:46:24 +0100

    Added basic layout for Test

--- 04_routing/043_country_news/src/components/Base.jsx
+++ 04_routing/043_country_news/src/components/Base.jsx
@@ -3,13 +3,17 @@ var React = require('react');
 var Base = React.createClass({
     render: function() {
         return (
-            <div className="page-base-holder">
-                <div className="page-base-header">
+            <div className="page-base-outer-container">
+                <div className="page-base-header-bg">
                 </div>
-                <div className="page-base-body">
-                    {this.props.children}
-                </div>
-                <div className="page-base-footer">
+                <div className="container page-base-holder">
+                    <div className="page-base-header">
+                    </div>
+                    <div className="page-base-body">
+                        {this.props.children}
+                    </div>
+                    <div className="page-base-footer">
+                    </div>
                 </div>
             </div>
         );
