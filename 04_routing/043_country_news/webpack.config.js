73c39f1dc41287c1d6b52aa30570ae280b759017
Viet Le <vietlq85@gmail.com>
2016-08-11 23:05:04 +0100

    Successfully added PROD mode

--- 04_routing/043_country_news/webpack.config.js
+++ 04_routing/043_country_news/webpack.config.js
@@ -4,6 +4,13 @@ var path = require('path');
 var BUILD_DIR = path.resolve(__dirname, 'public/js');
 var APP_DIR = path.resolve(__dirname, 'src');
 
+// Add this handful of plugins that optimize the build when we're in production
+var plugins = (process.env.NODE_ENV === 'production') ? [
+    new webpack.optimize.DedupePlugin(),
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.optimize.UglifyJsPlugin()
+] : [];
+
 var config = {
     entry: APP_DIR + '/main.jsx',
     output: {
@@ -19,6 +26,7 @@ var config = {
             }
         ]
     },
+    plugins: plugins,
 };
 
 module.exports = config;
