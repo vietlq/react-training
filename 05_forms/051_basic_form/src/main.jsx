944054876512d342ab3be85d485f31da1c02a5f3
Viet Le <vietlq85@gmail.com>
2016-08-18 06:50:31 +0100

    Renamed

--- 05_forms/051_basic_form/src/main.jsx
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import {render} from 'react-dom';
-import HelloWorld from './components/HelloWorld.jsx';
-
-render(<HelloWorld />, document.getElementById('main-container'));
