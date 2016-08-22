75c0ac445481d56693e35544818ca2cc7b3df214
Viet Le <vietlq85@gmail.com>
2016-08-22 20:36:28 +0100

    Added references

--- 07_nodejs/073_nodejs_weather/README.md
+++ 07_nodejs/073_nodejs_weather/README.md
@@ -2,6 +2,8 @@
 npm install -g nodemon
 ```
 
+https://github.com/mzabriskie/axios
+
 http://stackoverflow.com/questions/30773756/is-it-okay-to-use-babel-node-in-production
 
 http://babeljs.io/docs/plugins/#presets
@@ -39,3 +41,59 @@ imperial api.openweathermap.org/data/2.5/find?q=London&units=imperial
 http://api.openweathermap.org/data/2.5/forecast/daily?id=524901&lang=zh_cn
 
 http://openweathermap.org/forecast5
+
+https://erikflowers.github.io/weather-icons/
+
+http://autocomplete-js.com/
+
+https://github.com/autocompletejs/autocomplete.js
+
+https://twitter.github.io/typeahead.js/
+
+https://twitter.github.io/typeahead.js/examples/
+
+http://blog.goguardian.com/nerds/chrome-extensions-in-react
+
+https://www.codementor.io/reactjs/tutorial/building-a-chrome-extension-reactjs-broccoli-sass
+
+https://react.rocks/tag/Chrome_Extension
+
+http://michelletilley.net/2014/02/24/creating-chrome-extensions-with-react.html
+
+https://www.npmjs.com/package/react-chrome-extension-boilerplate
+
+http://stackoverflow.com/questions/25121144/alternating-row-colors-in-bootstrap-3-no-table
+
+```
+/*
+Since you are using bootstrap and you want alternating row colors for every screen sizes you need to write separate style rules for all the screen sizes.
+*/
+
+/* For small screen */
+.row :nth-child(even){
+  background-color: #dcdcdc;
+}
+.row :nth-child(odd){
+  background-color: #aaaaaa;
+}
+
+/* For medium screen */    
+@media (min-width: 768px) {
+    .row :nth-child(4n), .row :nth-child(4n-1) {
+        background: #dcdcdc;
+    }
+    .row :nth-child(4n-2), .row :nth-child(4n-3) {
+        background: #aaaaaa;
+    }
+}
+
+/* For large screen */
+@media (min-width: 992px) {
+    .row :nth-child(6n), .row :nth-child(6n-1), .row :nth-child(6n-2) {
+        background: #dcdcdc;
+    }
+    .row :nth-child(6n-3), .row :nth-child(6n-4), .row :nth-child(6n-5) {
+        background: #aaaaaa;
+    }
+}
+```
--- 07_nodejs/073_nodejs_weather/server.js
+++ 07_nodejs/073_nodejs_weather/server.js
@@ -1,4 +1,57 @@
-'use strict';var _compression=require('compression');var _compression2=_interopRequireDefault(_compression);var _express=require('express');var _express2=_interopRequireDefault(_express);var _bodyParser=require('body-parser');var _bodyParser2=_interopRequireDefault(_bodyParser);var _path=require('path');var _path2=_interopRequireDefault(_path);function _interopRequireDefault(obj){return obj&&obj.__esModule?obj:{default:obj}}var app=(0,_express2.default)();// Middleware
-app.use((0,_compression2.default)());app.use(_bodyParser2.default.json());app.use(_bodyParser2.default.urlencoded({extended:false}));var ROOT_DIR=__dirname;var STATIC_DIR=_path2.default.join(ROOT_DIR,'public');// Serve our static stuff like index.css
-app.use(_express2.default.static(STATIC_DIR));var animals=['cat','dog','Pokemon','Pikachu','devious turtle','bear','hippo','ox'];var birds=['Phoenix','Kungfu Crane','Eagle','Swallow','Swan'];var secrets=['Death Star','Treasure','Gold','Lost City'];app.get('/animals',function(req,res){res.send(animals)});app.get('/birds',function(req,res){res.send(birds)});app.get('/secrets',function(req,res){res.send(secrets)});// Send all requests to index.html so browserHistory in React Router works
-app.get('*',function(req,res){res.sendFile(_path2.default.join(STATIC_DIR,'index.html'))});app.listen(6969);
+'use strict';
+
+var _compression = require('compression');
+
+var _compression2 = _interopRequireDefault(_compression);
+
+var _express = require('express');
+
+var _express2 = _interopRequireDefault(_express);
+
+var _bodyParser = require('body-parser');
+
+var _bodyParser2 = _interopRequireDefault(_bodyParser);
+
+var _path = require('path');
+
+var _path2 = _interopRequireDefault(_path);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var app = (0, _express2.default)();
+
+// Middleware
+app.use((0, _compression2.default)());
+app.use(_bodyParser2.default.json());
+app.use(_bodyParser2.default.urlencoded({ extended: false }));
+
+var ROOT_DIR = __dirname;
+var STATIC_DIR = _path2.default.join(ROOT_DIR, 'public');
+
+// Serve our static stuff like index.css
+app.use(_express2.default.static(STATIC_DIR));
+
+var animals = ['cat', 'dog', 'Pokemon', 'Pikachu', 'devious turtle', 'bear', 'hippo', 'ox'];
+
+var birds = ['Phoenix', 'Kungfu Crane', 'Eagle', 'Swallow', 'Swan'];
+
+var secrets = ['Death Star', 'Treasure', 'Gold', 'Lost City'];
+
+app.get('/animals', function (req, res) {
+    res.send(animals);
+});
+
+app.get('/birds', function (req, res) {
+    res.send(birds);
+});
+
+app.get('/secrets', function (req, res) {
+    res.send(secrets);
+});
+
+// Send all requests to index.html so browserHistory in React Router works
+app.get('*', function (req, res) {
+    res.sendFile(_path2.default.join(STATIC_DIR, 'index.html'));
+});
+
+app.listen(6969);
