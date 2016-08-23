71da6bbcbc8cf1df8ba3cc55df76ef56fc1a2225
Viet Le <vietlq85@gmail.com>
2016-08-23 06:56:08 +0100

    Remove clutter from App.jsx

--- 07_nodejs/073_nodejs_weather/src/server/App.jsx
+++ 07_nodejs/073_nodejs_weather/src/server/App.jsx
@@ -16,30 +16,6 @@ var STATIC_DIR = path.join(ROOT_DIR, 'public');
 // Serve our static stuff like index.css
 app.use(express.static(STATIC_DIR));
 
-const animals = [
-    'cat', 'dog', 'Pokemon', 'Pikachu', 'devious turtle', 'bear', 'hippo', 'ox',
-]
-
-const birds = [
-    'Phoenix', 'Kungfu Crane', 'Eagle', 'Swallow', 'Swan',
-]
-
-const secrets = [
-    'Death Star', 'Treasure', 'Gold', 'Lost City',
-]
-
-app.get('/animals', function(req, res) {
-    res.send(animals);
-});
-
-app.get('/birds', function(req, res) {
-    res.send(birds);
-});
-
-app.get('/secrets', function(req, res) {
-    res.send(secrets);
-});
-
 // Send all requests to index.html so browserHistory in React Router works
 app.get('*', function (req, res) {
     res.sendFile(path.join(STATIC_DIR, 'index.html'));
