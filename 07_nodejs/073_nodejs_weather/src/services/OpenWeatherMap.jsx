5337bc56973247d290efb91422d11c2f5037634e
Viet Le <vietlq85@gmail.com>
2016-08-27 09:17:24 +0100

    Added getWeatherByCityId

--- 07_nodejs/073_nodejs_weather/src/services/OpenWeatherMap.jsx
+++ 07_nodejs/073_nodejs_weather/src/services/OpenWeatherMap.jsx
@@ -19,11 +19,33 @@ var makeForecastParams = function(input) {
     return params;
 }
 
+var makeWeatherParams = function(input) {
+    var params = {};
+
+    params.appid = input.appId || 'INVALID';
+    params.id = input.cityId || 524901; // Moscow
+    params.units = input.units || 'metric'; // Metric units by default
+    params.mode = 'json'; // We only use JSON format
+
+    return params;
+}
+
 var OpenWeatherMap = {
     getDailyForecastByCityId: function(input) {
         var params = makeForecastParams(input);
 
         return axios.get(DAILY_FORECAST_URL, {
+            params: params
+        }).then(function (response) {
+            return response.data;
+        }).catch(function (error) {
+            console.log('Error: ' + error);
+        });
+    },
+    getWeatherByCityId: function(input) {
+        var params = makeWeatherParams(input);
+
+        return axios.get(WEATHER_URL, {
             params: params
         }).then(function (response) {
             return response.data;
